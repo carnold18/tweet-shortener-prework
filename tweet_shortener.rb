adc9db3f9122c38042f7c0ff6bedc6a907e7487d
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-13 15:46:06 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,5 +40,7 @@ end
 def shortened_tweet_truncator(string)
   if word_substituter(string).length > 140
     word_substituter(string)[0..136] + "..."
+  else
+    word_substituter(string)
   end
 end
